3b17e70f5ee211a3b8ae4092886aa5d9a482508e
SILVA-02 <144107254+HAFIZ-02@users.noreply.github.com>
2024-06-10 12:01:33 +0700

    Add files via upload

---
+++ Jobsheet 1/latihan/Untitled1.cpp
@@ -0,0 +1,9 @@
+//cretaed by Hafiz Hafrienda 23343067
+
+#include <stdio.h>
+
+int main()
+{
+	printf("\n welcome to the word of C");//print the message on the screen
+	return 0;//return a value 0 to the operating system 
+}
